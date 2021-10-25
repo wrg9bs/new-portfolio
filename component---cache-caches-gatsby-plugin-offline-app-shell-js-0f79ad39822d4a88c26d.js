636b4d5ef0e3412b9b4de435756ffe53165f8a2b
wrg9bs <boatzir@yahoo.com>
2021-10-25 19:28:10 +1300

    Updates

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-0f79ad39822d4a88c26d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknew_portfolio=self.webpackChunknew_portfolio||[]).push([[306],{91930:function(e,t,n){var u=n(95318);t.__esModule=!0,t.default=void 0;var o=u(n(85354)),r=u(n(67294)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component),f=l;t.default=f}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-0f79ad39822d4a88c26d.js.map
