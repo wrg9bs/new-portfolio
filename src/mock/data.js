4e04c35de662e212432ea67a0802cb81e004606f
wrg9bs <boatzir@yahoo.com>
2021-10-25 20:03:39 +1300

    changed headData information

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Bo-at Portfolio', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  description: 'Bo-at portfolio built with gatsby', // e.g: Welcome to my website
 };
 
 // HERO DATA
